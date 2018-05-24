31f50b919284e10b22588377fc70e53de08945d5
Arsenije Karpic <arsenije.karpic@gmail.com>
2018-05-24 23:51:25 +0200

    Popravljena messages komponenta

--- src/app/reservations/messages/messages.component.ts
+++ src/app/reservations/messages/messages.component.ts
@@ -2,7 +2,7 @@ import { MessagesService } from './../../services/messages.service';
 import { MessageCreation } from './../../models/messageCreation.model';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { DataService } from './../../services/dataService.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MessageView } from '../../models/messageView.model';
 import { ReservationView } from '../../models/reservationView.model';
 
@@ -15,7 +15,8 @@ export class MessagesComponent implements OnInit {
   messages: MessageView[];
   reservationView: ReservationView;
   newMessage: MessageCreation = new MessageCreation('','',0);
-  //@ViewChild('messageBox') messageBox
+  @ViewChild('messageBox') messageBox: ElementRef;
+
   getMessages() {
     this.dataService.currentReservationView.subscribe(
       reservation => {
@@ -23,9 +24,26 @@ export class MessagesComponent implements OnInit {
         this.messages = reservation.messages;
 
         //sortinranje poruka po datumu
-
+        this.messages.sort(
+          (a, b) => new Date(this.parseDate(a.dateSent.toString())).getTime() - new Date(this.parseDate(b.dateSent.toString())).getTime()
+        )
       }
     )
+  }
+
+  parseDate(dateString: string): Date {
+    var yearArray: string[] = dateString.split(" ")[0].split("-");
+    var returnDate = new Date();
+    returnDate.setDate(+yearArray[0]);
+    returnDate.setMonth(+yearArray[1]);
+    returnDate.setFullYear(+yearArray[2]);
+
+    var timeArray: string[] = dateString.split(" ")[1].split(":");
+    returnDate.setHours(+timeArray[0]);
+    returnDate.setMinutes(+timeArray[1]);
+    returnDate.setSeconds(+timeArray[2]);
+
+    return returnDate;
   }
 
   sendMessage() {
@@ -42,6 +60,7 @@ export class MessagesComponent implements OnInit {
     this.messagesService.insertMessage(this.newMessage).subscribe(
       (createdMessage) => {
         this.messages.push(createdMessage);
+        this.messageBox.nativeElement.value = '';
       }
     );
   }
